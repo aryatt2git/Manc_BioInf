4bb9a8aa19fe35576d48af9337042a1bb9d713ba
aryatt2git <arjunryatt@hotmail.co.uk>
2025-09-09 16:04:07 +0100

    test the workflow in Actions.

--- PythonProgramming/tests/test_chunk_sequence.py
+++ PythonProgramming/tests/test_chunk_sequence.py
@@ -3,7 +3,7 @@ from modules.chunk_sequence import chunk_sequence
 
 def test_chunk_sequence():
 
-    sequence = 'aggagtaagcccttgcaactggaaatacacccattg'
+    sequence = 'aggagtaagcccttgcaactggaaatacacccattg' #a change
     chunk_length = 3
 
     chunk_sequence(sequence, chunk_length)
