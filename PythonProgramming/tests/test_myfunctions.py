a911eb7aa84f1ca71c3f8e6199ce31bc76b42217
aryatt2git <arjunr@DG5BP4W2.oxnet.nhs.uk>
2025-09-09 14:49:12 +0100

    test the pytest

--- PythonProgramming/tests/test_myfunctions.py
+++ PythonProgramming/tests/test_myfunctions.py
@@ -3,7 +3,7 @@ from modules.chunk_sequence import chunk_sequence
 
 def test_chunk_sequence():
 
-    sequence = 'aggagtaagcccttgcaactggaaatacacccattg'
+    sequence = 'aggagtaagccfttgcaactggaaatacacccattg'
     chunk_length = 3
 
     chunk_sequence(sequence, chunk_length)
