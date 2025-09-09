feebfe21981597cee5abc0c61364e857cd7115f1
aryatt2git <arjunr@DG5BP4W2.oxnet.nhs.uk>
2025-09-09 14:37:02 +0100

    Creating coverage files

--- PythonProgramming/tests/test_myfunctions.py
+++ PythonProgramming/tests/test_myfunctions.py
@@ -3,4 +3,15 @@ from modules.chunk_sequence import chunk_sequence
 
 def test_chunk_sequence():
 
-    chunk_sequence('aggagtaagcccttgcaactggaaatacacccattg', 3)
+    sequence = 'aggagtaagcccttgcaactggaaatacacccattg'
+    chunk_length = 3
+
+    chunk_sequence(sequence, chunk_length)
+
+    for n in sequence:
+        assert n in ['a', 'c', 'g', 't', 'A', 'C', 'G', 'T']
+
+    assert len(sequence) % chunk_length == 0
+
+    assert (type(chunk_length) == int)
+
