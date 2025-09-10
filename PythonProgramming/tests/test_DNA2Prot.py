25cdfbb58a50242ce13381ced4fa826a85a5f664
aryatt2git <arjunryatt@hotmail.co.uk>
2025-09-10 09:31:45 +0100

    more tests for pytest

---
+++ PythonProgramming/tests/test_DNA2Prot.py
@@ -0,0 +1,14 @@
+import pytest
+from modules.DNA2Prot import DNA2Prot
+
+def test_DNA2Prot():
+
+    sequence = 'aggagtaagcccttfgcaactggaaatacacccattg'
+
+    DNA2Prot(sequence)
+
+    assert type(sequence) == str
+
+    for n in sequence:
+        assert n in ['a', 'c', 'g', 't', 'A', 'C', 'G', 'T']
+    
--- PythonProgramming/tests/test_chunk_sequence.py
+++ PythonProgramming/tests/test_chunk_sequence.py
@@ -4,14 +4,16 @@ from modules.chunk_sequence import chunk_sequence
 def test_chunk_sequence():
 
     sequence = 'aggagtaagcccttgcaactggaaatacacccattg'
+
     chunk_length = 3
 
     chunk_sequence(sequence, chunk_length)
 
+    assert type(sequence) == str
+
     for n in sequence:
         assert n in ['a', 'c', 'g', 't', 'A', 'C', 'G', 'T']
 
-    assert len(sequence) % chunk_length == 0
-
     assert (type(chunk_length) == int)
 
+    assert len(sequence) % chunk_length == 0
