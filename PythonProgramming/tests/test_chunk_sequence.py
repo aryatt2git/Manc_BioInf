e611b517db7faae9a9ecb6a6762723a1a9fd986b
aryatt2git <arjunryatt@hotmail.co.uk>
2025-10-07 10:04:10 +0100

    updated chunk_sequence pytest.

--- PythonProgramming/tests/test_chunk_sequence.py
+++ PythonProgramming/tests/test_chunk_sequence.py
@@ -3,16 +3,22 @@ from modules.chunk_sequence import chunk_sequence
 #from utilities.pytest_checks import test_other_data_types
 
 @pytest.mark.parametrize('sequence, chunk_length, expected', [
-        ('ATCGGCTA', 3, 'ATC GGC TA'),      # checks normal function
-        ('atcggcta', 3, 'ATC GGC TA'),      # maybe function converts to uppercase
-        ('ATBXZ', 2, 'AssertionError: B at position 3 in the sequence is not a nucleotide.'),            # depends on how invalid chars handled
-        ('', 3, ''),                        # empty input returns empty list
-        ('ATCG', 10, 'ATCG'),               # chunk size > seq length
-        ('ATCG', 4, 'ATCG'),                # chunk size == seq length
-        ('ATCG', 1, 'A T C G'),             # chunk size = 1
+        ('ATCGGCTA', 3, 'ATC GGC TA'),                                                   # checks normal function
+        ('atcggcta', 3, 'ATC GGC TA'),                                                   # maybe function converts to uppercase
+        ('ATBXZ', 2, 'B at position 3 in the sequence is not a nucleotide.'),            # depends on how invalid chars handled
+        ('', 3, ''),                                                                     # empty input returns empty list
+        ('ATCG', 10, 'ATCG'),                                                            # chunk size > seq length
+        ('ATCG', 4, 'ATCG'),                                                             # chunk size == seq length
+        ('ATCG', 1, 'A T C G'),                                                          # chunk size = 1
 ])
 
 def test_chunk_sequence(sequence, chunk_length, expected):
+
+    with pytest.raises(AssertionError) as exc_info:
+
+        chunk_sequence(sequence, chunk_length)
+
+    assert str(exc_info.value) == expected
 
     output = chunk_sequence(sequence, chunk_length)
 
